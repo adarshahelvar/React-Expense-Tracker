7ffe37fc62c6e8345ebed05db1058617cfcf3623
Adarsha A <adarshaadi1997@gmail.com>
2023-03-14 18:17:19 +0530

    separate file for date component

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ function App() {
       id: "01",
       title: "Car Insurance",
       amount: 296.15,
-      date: new Date(20 , 3 , 2023),
+      date: new Date(2013 , 3 , 15),
     },
     {
       id: "02",
       title: "Bike Insurance",
       amount: 126.15,
-      date: new Date(18 , 6 , 2023),
+      date: new Date(2023 , 6 , 12),
     },
     {
       id: "03",
       title: "Life Insurance",
       amount: 4256.15,
-      date: new Date(7 , 9 , 2023),
+      date: new Date(2023 , 9 , 7),
     },
     {
       id: "04",
       title: "Health Insurance",
       amount: 1221.15,
-      date: new Date(29 , 12 , 2023),
+      date: new Date(2023 , 11 , 18),
     },
   ];
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./expenseItems.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-    // console.log(props);
+  console.log(props);
+  // { props.date.toISOString().slice(0,10)}
 
   return (
     <div className="expense-item">
-      <div> { props.date.toISOString()} </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2> {props.title}</h2>
-        <div className="expense-item__price">$ { props.amount }</div>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
